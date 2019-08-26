5638d189dd8fe0ff12c96ee5d732e0580fbb9206
Mevin Dhunnooa <mevin.dhunnooa@sc-mauritius.com>
2019-08-26 18:02:34 +0400

    run on travis

---
+++ tests/travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+npm install -g cordova
+npm install
+
+# lint
+# npm run lint
+
+# run tests appropriate for platform
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    sudo gem install cocoapods
+    npm install -g ios-sim ios-deploy
+    npm run test:ios
+fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
+    emulator -avd test -no-audio -no-window &
+    android-wait-for-emulator
+    npm run test:android
+fi
