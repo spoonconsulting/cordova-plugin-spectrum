a2482c7bbf84287666be8fc208386db45299a9ba
Mevin Dhunnooa <mevin.dhunnooa@sc-mauritius.com>
2019-08-27 13:40:17 +0400

    remove fat arrow

--- tests/tests.js
+++ tests/tests.js
@@ -1,115 +1,117 @@
-
-// exports.defineAutoTests = () => {
-//   describe('Spectrum', () => {
-//     function copyFileToDataDirectory (fileName) {
-//       return new Promise(function (resolve, reject) {
-//         console.log('Copying :' + fileName + ' ' + cordova.file.applicationDirectory)
-//         window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + fileName, function (fileEntry) {
-//           window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directory) {
-//             console.log('copy to ', directory.nativeURL)
-//             fileEntry.copyTo(directory, fileName, function () {
-//               resolve((cordova.file.dataDirectory + fileName))
-//             },
-//             function (err) {
-//               console.log(err)
-//               reject(err)
-//             })
-//           }, reject)
-//         }, reject)
-//       })
-//     }
-
-//     function getFileSize (imageUri) {
-//       return new Promise(function (resolve, reject) {
-//         window.resolveLocalFileSystemURI(imageUri,
-//           function (fileEntry) {
-//             fileEntry.file(function (fileObj) {
-//               resolve(fileObj.size / (1024 * 1024))
-//             },
-//             reject)
-//           }, reject)
-//       })
-//     }
-
-//     function deleteFile (fileName) {
-//       return new Promise(function (resolve, reject) {
-//         window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dir) {
-//           dir.getFile(fileName, {
-//             create: false
-//           }, function (fileEntry) {
-//             fileEntry.remove(resolve, reject, reject)
-//           })
-//         }, reject)
-//       })
-//     }
-
-//     it('exposes SpectrumManager globally', () => {
-//       expect(SpectrumManager).toBeDefined()
-//     })
-
-//     it('should have compressImage function', () => {
-//       expect(SpectrumManager.compressImage).toBeDefined()
-//     })
-
-//     it('returns an error if no argument is given', (done) => {
-//       SpectrumManager.compressImage(null, () => {}, err => {
-//         expect(err).toBeDefined()
-//         expect(err.message).toBe('missing options')
-//         done()
-//       })
-//     })
-
-//     it('returns an error if sourcePath is not given', (done) => {
-//       SpectrumManager.compressImage({}, () => {}, err => {
-//         expect(err).toBeDefined()
-//         expect(err.message).toBe('sourcePath is missing')
-//         done()
-//       })
-//     })
-
-//     it('returns an error if sourcePath is invalid', (done) => {
-//       SpectrumManager.compressImage({
-//         sourcePath: 'invalid url'
-//       }, () => {}, err => {
-//         expect(err).toBeDefined()
-//         done()
-//       })
-//     })
-
-//     it('compresses an image', (done) => {
-//       var sampleFile = 'tree.jpg'
-//       copyFileToDataDirectory(sampleFile).then(path => {
-//         getFileSize(path).then(originalSize => {
-//           SpectrumManager.compressImage({
-//             sourcePath: path
-//           }, () => {
-//             getFileSize(path).then(newSize => {
-//               expect(newSize).toBeGreaterThan(0)
-//               expect(newSize).toBeLessThan(originalSize)
-//               deleteFile(sampleFile).then(done)
-//             })
-//           }, err => console.err(err))
-//         })
-//       })
-//     })
-
-//     it('preserves exif on compressed image', (done) => {
-//       var sampleFile = 'tree.jpg'
-//       copyFileToDataDirectory(sampleFile).then(path => {
-//         CordovaExif.readData(path, function (exif) {
-//           expect(Object.keys(exif).length).toBeGreaterThan(0)
-//           expect(exif.Make).toBe('google')
-//           expect(exif.ShutterSpeedValue).toBe(11.22)
-//           deleteFile(sampleFile).then(done)
-//         })
-//       })
-//     })
-//   })
-// }
+/* global cordova */
+/* global SpectrumManager */
+/* global CordovaExif */
 exports.defineAutoTests = function () {
-  describe('Device Information (window.device)', function () {
-    it('should exist', function () {
-      expect(window.device).toBeDefined()
+  describe('Spectrum', function () {
+    function copyFileToDataDirectory (fileName) {
+      return new Promise(function (resolve, reject) {
+        console.log('Copying :' + fileName + ' ' + cordova.file.applicationDirectory)
+        window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + fileName, function (fileEntry) {
+          window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directory) {
+            console.log('copy to ', directory.nativeURL)
+            fileEntry.copyTo(directory, fileName, function () {
+              resolve((cordova.file.dataDirectory + fileName))
+            },
+            function (err) {
+              console.log(err)
+              reject(err)
+            })
+          }, reject)
+        }, reject)
+      })
+    }
+
+    function getFileSize (imageUri) {
+      return new Promise(function (resolve, reject) {
+        window.resolveLocalFileSystemURI(imageUri,
+          function (fileEntry) {
+            fileEntry.file(function (fileObj) {
+              resolve(fileObj.size / (1024 * 1024))
+            },
+            reject)
+          }, reject)
+      })
+    }
+
+    function deleteFile (fileName) {
+      return new Promise(function (resolve, reject) {
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dir) {
+          dir.getFile(fileName, {
+            create: false
+          }, function (fileEntry) {
+            fileEntry.remove(resolve, reject, reject)
+          })
+        }, reject)
+      })
+    }
+
+    it('exposes SpectrumManager globally', function () {
+      expect(SpectrumManager).toBeDefined()
+    })
+
+    it('should have compressImage function', () => {
+      expect(SpectrumManager.compressImage).toBeDefined()
+    })
+
+    it('returns an error if no argument is given', (done) => {
+      SpectrumManager.compressImage(null, () => {}, err => {
+        expect(err).toBeDefined()
+        expect(err.message).toBe('missing options')
+        done()
+      })
+    })
+
+    it('returns an error if sourcePath is not given', (done) => {
+      SpectrumManager.compressImage({}, () => {}, err => {
+        expect(err).toBeDefined()
+        expect(err.message).toBe('sourcePath is missing')
+        done()
+      })
+    })
+
+    it('returns an error if sourcePath is invalid', (done) => {
+      SpectrumManager.compressImage({
+        sourcePath: 'invalid url'
+      }, () => {}, err => {
+        expect(err).toBeDefined()
+        done()
+      })
+    })
+
+    it('compresses an image', (done) => {
+      var sampleFile = 'tree.jpg'
+      copyFileToDataDirectory(sampleFile).then(path => {
+        getFileSize(path).then(originalSize => {
+          SpectrumManager.compressImage({
+            sourcePath: path
+          }, () => {
+            getFileSize(path).then(newSize => {
+              expect(newSize).toBeGreaterThan(0)
+              expect(newSize).toBeLessThan(originalSize)
+              deleteFile(sampleFile).then(done)
+            })
+          }, err => console.err(err))
+        })
+      })
+    })
+
+    it('preserves exif on compressed image', (done) => {
+      var sampleFile = 'tree.jpg'
+      copyFileToDataDirectory(sampleFile).then(path => {
+        CordovaExif.readData(path, function (exif) {
+          expect(Object.keys(exif).length).toBeGreaterThan(0)
+          expect(exif.Make).toBe('google')
+          expect(exif.ShutterSpeedValue).toBe(11.22)
+          deleteFile(sampleFile).then(done)
+        })
+      })
     })
   })
 }
+// exports.defineAutoTests = function () {
+//   describe('Device Information (window.device)', function () {
+//     it('should exist', function () {
+//       expect(window.device).toBeDefined()
+//     })
+//   })
+// }
